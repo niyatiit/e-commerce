4c0c7b60a83f8ee64cb011ba0a0d39255b0895da
niyatiit <niyatipatel0701@gmail.com>
2026-01-04 12:24:28 +0530

    Day 1:- backend setup and overview

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,18 @@
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("Error :--- ", error);
+  }
+};
+const registerUser = async (req, res) => {
+  res.json({ msg: "Register Api is working " });
+};
+
+const adminLogin = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("Error :--- ", error);
+  }
+};
+
+export { loginUser, registerUser, adminLogin };
---
+++ Server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/user.route.js'
+
+
+// App Config 
+const app = express()
+const port = process.env.PORT
+connectDB()
+connectCloudinary()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+// Api Endpoints
+app.use('/api/user' , userRouter)
+
+
+app.get("/" ,(req,res)=>{
+    res.send("API is working properly")
+})
+
+
+app.listen(port , ()=>{
+    console.log(`Server running on port ${port}`)
+})
